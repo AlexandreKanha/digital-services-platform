dd0a17f48f31e718f2fed0b48752dcb7a36b1c58
AlexandreKanha <alexandrelskanha@outlook.com>
2026-01-21 22:55:52 -0300

    Security

---
+++ backend/digitalservices/src/main/java/br/com/alexandre/digitalservices/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package br.com.alexandre.digitalservices.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @PostConstruct
+    public void init() {
+        System.out.println(">>> SECURITY CONFIG CARREGADA <<<");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .httpBasic(httpBasic -> httpBasic.disable())
+            .formLogin(formLogin -> formLogin.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/users/**").permitAll()
+                .requestMatchers("/actuator/**").permitAll()
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+}
